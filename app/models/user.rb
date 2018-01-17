990dc4caf6ccb2ad663e01ee0923c4ff5a7718a2
Amer <amer.jaganjac@yahoo.com>
2018-01-16 21:44:16 -0500

    mijn locale code aangepast voor deploy

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :articles
+  before_save { self.email = email.downcase }
+  
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+  
+  
+end
